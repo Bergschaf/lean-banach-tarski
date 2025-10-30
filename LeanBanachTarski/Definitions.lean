b4f9a901e297988755c65dff32d58434f1caf847
Bergschaf <christian.k@netcom-mail.de>
2025-10-30 11:06:43 +0100

    sl 3 R

--- LeanBanachTarski/Definitions.lean
+++ LeanBanachTarski/Definitions.lean
@@ -1,6 +1,6 @@
 import Mathlib.Analysis.InnerProductSpace.EuclideanDist
-
-abbrev SO_3 := Matrix.specialOrthogonalGroup (Fin 3) ℝ
+import Mathlib.LinearAlgebra.Matrix.SpecialLinearGroup
+import Mathlib.GroupTheory.FreeGroup.IsFreeGroup
 
 noncomputable section
 def matrix_a   : Matrix (Fin 3) (Fin 3) Real := !![1, 0, 0; 0, 1/3, -2/3*Real.sqrt 2; 0, 2/3*Real.sqrt 2, 1/3]
@@ -11,7 +11,7 @@ def matrix_one : Matrix (Fin 3) (Fin 3) Real := 1
 end noncomputable section
 
 
-theorem matrix_a_det_neq_zero : Matrix.det matrix_a ≠ 0 := by
+theorem matrix_a_det_eq_one : Matrix.det matrix_a = 1 := by
   rw [matrix_a, Matrix.det_fin_three]
   norm_num
   simp only [one_div, Fin.isValue, Matrix.cons_val, Matrix.cons_val_one, neg_mul, sub_neg_eq_add,
@@ -19,8 +19,7 @@ theorem matrix_a_det_neq_zero : Matrix.det matrix_a ≠ 0 := by
   ring_nf
   norm_num
 
-
-theorem matrix_a'_det_neq_zero : Matrix.det matrix_a' ≠ 0 := by
+theorem matrix_a'_det_eq_one : Matrix.det matrix_a' = 1 := by
   rw [matrix_a', Matrix.det_fin_three]
   norm_num
   simp only [one_div, Fin.isValue, Matrix.cons_val, Matrix.cons_val_one, mul_neg, sub_neg_eq_add,
@@ -28,7 +27,7 @@ theorem matrix_a'_det_neq_zero : Matrix.det matrix_a' ≠ 0 := by
   ring_nf
   norm_num
 
-theorem matrix_b_det_neq_zero : Matrix.det matrix_b ≠ 0 := by
+theorem matrix_b_det_eq_one : Matrix.det matrix_b = 1 := by
   rw [matrix_b, Matrix.det_fin_three]
   norm_num
   simp only [one_div, Fin.isValue, Matrix.cons_val, Matrix.cons_val_one, mul_one, mul_zero,
@@ -36,7 +35,7 @@ theorem matrix_b_det_neq_zero : Matrix.det matrix_b ≠ 0 := by
   ring_nf
   norm_num
 
-theorem matrix_b'_det_neq_zero : Matrix.det matrix_b' ≠ 0 := by
+theorem matrix_b'_det_eq_one : Matrix.det matrix_b' = 1 := by
   rw [matrix_b', Matrix.det_fin_three]
   norm_num
   simp only [one_div, Fin.isValue, Matrix.cons_val, Matrix.cons_val_one, mul_one, mul_zero,
@@ -44,18 +43,27 @@ theorem matrix_b'_det_neq_zero : Matrix.det matrix_b' ≠ 0 := by
   ring_nf
   norm_num
 
-theorem matrix_one_det_neq_zero : Matrix.det matrix_one ≠ 0 := by
+theorem matrix_one_det_eq_one : Matrix.det matrix_one = 1 := by
   rw [matrix_one, Matrix.det_fin_three]
   simp
 
+open MatrixGroups
+
 
 noncomputable section
-def gl_a   : GL (Fin 3) Real := Matrix.GeneralLinearGroup.mkOfDetNeZero matrix_a matrix_a_det_neq_zero
-def gl_a'  : GL (Fin 3) Real := Matrix.GeneralLinearGroup.mkOfDetNeZero matrix_a' matrix_a'_det_neq_zero
-def gl_b   : GL (Fin 3) Real := Matrix.GeneralLinearGroup.mkOfDetNeZero matrix_b matrix_b_det_neq_zero
-def gl_b'  : GL (Fin 3) Real := Matrix.GeneralLinearGroup.mkOfDetNeZero matrix_b' matrix_b'_det_neq_zero
-def gl_one : GL (Fin 3) Real := Matrix.GeneralLinearGroup.mkOfDetNeZero matrix_one matrix_one_det_neq_zero
+def sl_a : SL(3, ℝ) := ⟨matrix_a, matrix_a_det_eq_one⟩
+def sl_a' : SL(3, ℝ) := ⟨matrix_a', matrix_a'_det_eq_one⟩
+def sl_b : SL(3, ℝ) := ⟨matrix_b, matrix_b_det_eq_one⟩
+def sl_b' : SL(3, ℝ) := ⟨matrix_b', matrix_b'_det_eq_one⟩
+def sl_one : SL(3, ℝ) := ⟨matrix_one, matrix_one_det_eq_one⟩
 end noncomputable section
 
+
+-- TOOD mathlib?? closure von ... ist free Group.
+def F_2 : Subgroup SL(3, ℝ) := Subgroup.closure {sl_a, sl_b, sl_a', sl_b'}
+
+instance : IsFreeGroup F_2 where
+  nonempty_basis := by
+    sorry
 
 abbrev ℝ_3 := Fin 3 -> ℝ
