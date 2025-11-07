d642a799ba90032de978bc718b28995a0fb3b1bf
Bergschaf <christian.k@netcom-mail.de>
2025-11-07 14:01:46 +0100

    c

--- LeanBanachTarski/Rotations_F_2.lean
+++ LeanBanachTarski/Rotations_F_2.lean
@@ -1,4 +1,5 @@
 import LeanBanachTarski.Definitions
+import Mathlib.Tactic.Rify
 
 open Matrix FreeGroup
 
@@ -9,10 +10,36 @@ theorem inj : Function.Injective F_2_to_Rots := by
   apply Iff.intro
   . intro h
     obtain ⟨a,b,c,h1⟩ := Free_Rots_mul_vec w
-    contrapose h
+    by_contra hc
     -- irgendwas mit a = c = 0 oder b = 1 genau dann wenn w = 0 (weil length dann 0)
-
-
+    have h_len : w.toWord.length > 0 := by
+      contrapose h
+      simp_all only [Nat.succ_eq_add_one, Nat.reduceAdd, one_div, smul_cons, smul_eq_mul,
+        smul_empty, gt_iff_lt, not_lt, nonpos_iff_eq_zero, List.length_eq_zero_iff,
+        toWord_eq_nil_iff, not_true_eq_false, not_false_eq_true]
+    have h1' := h1
+    have h1'' := h1
+    rw [h] at h1
+    simp at h1
+    simp [h1, h] at h1'
+    cases h1' with
+    | inl h =>
+      have hb : b = 3^ w.toWord.length := by
+        apply_fun ((3 ^ w.toWord.length) * .) at h
+        simp at h
+        simp_all only [gt_iff_lt, Nat.succ_eq_add_one, Nat.reduceAdd, OneMemClass.coe_one,
+          SpecialLinearGroup.coe_one, one_mulVec, Int.cast_zero, zero_mul, smul_cons, smul_eq_mul,
+          mul_zero, smul_empty, vecCons_inj, mul_eq_mul_right_iff, Int.cast_eq_zero, and_true,
+          true_and]
+        apply_fun ((3 ^ w.toWord.length) * .) at h
+        simp at h
+        rify
+        exact h.symm
+      clear h1 h
+      subst hb
+      sorry
+    | inr h => simp_all
+  simp_all
 
 #exit
 theorem exists_inv_fun : ∀ g ∈ Free_Rots, ∃ w : FreeGroup (Fin 2), F_2_to_Rots w = g := by
